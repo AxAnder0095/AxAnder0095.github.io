173ed8a4c26c0bdae8ea00be97cc067a6acb8208
Frenguin <104327737+Frenguin@users.noreply.github.com>
2025-02-20 00:13:38 -0600

    basename fix for black screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Page/Home.tsx";
-// import Navbar from "./Components/Navbar/Navbar.tsx";
-// import Footer from "./Components/Footer/Footer.tsx";
 
 function App() {
-
-
+    // make sure the basename in the Router matches the basename
+    // in the vite.config.ts file
+    // Can use basename="/portfolio/" or basename={import.meta.env.BASE_URL}
     return (
         <div className="main">
-            {/*<Navbar/>*/}
-            <Router>
+            <Router basename={import.meta.env.BASE_URL}>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                 </Routes>
             </Router>
-            {/*<Footer/>*/}
         </div>
     )
 }
